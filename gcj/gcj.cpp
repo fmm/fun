6a7e3621fcef3e6c45b016963a5ef2bcef779e20
Filipe Martins <filipex7@gmail.com>
2013-05-31 14:52:05 -0300

    gcj folder

---
+++ gcj/gcj.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define _ << " _ " <<
+#define dbg(x) cerr << #x << " == " << x << endl
+#define mp(x,y) make_pair((x),(y))
+#define pv(x,y) {for(typeof(y) z=(x);z!=(y);z++)cerr<<*z<<" ";cerr<<endl;}
+#define rep(x,y) for(int(x)=(0);(x)<int(y);++(x))
+#define x first
+#define y second
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pt;
+
+#if 0
+#define GENERATE 1
+#endif
+
+void read() {
+}
+
+void process() {
+}
+
+int main() {
+	int T;
+#ifdef GENERATE
+	unsigned int seed=time(0);
+	dbg(seed);
+	srand(seed);
+	T=50;
+	for(int testcase=1;testcase<=T;++testcase) {
+		fprintf(stderr,"Case #%d:",testcase);
+		// *generate input!
+		// BEGIN
+		// END
+#else
+		cin>>T;
+		for(int testcase=1;testcase<=T;++testcase) {
+			fprintf(stdout,"Case #%d:",testcase);
+			read();
+#endif
+		try {
+			process();
+		} catch(char const*exception) {
+			puts(exception);
+		}
+	}
+	return 0;
+}
+
